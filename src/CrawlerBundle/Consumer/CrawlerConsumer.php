46792b7cebcef8ebbbd387704135a3832fc65401
Baptiste Langlade <baptouuuu@gmail.com>
2015-12-09 08:12:53 +0100

    add crawler consumer

---
+++ src/CrawlerBundle/Consumer/CrawlerConsumer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CrawlerBundle\Consumer;
+
+use CrawlerBundle\Publisher;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+use Psr\Log\LoggerInterface;
+
+class CrawlerConsumer implements ConsumerInterface
+{
+    protected $publisher;
+    protected $logger;
+
+    public function __construct(Publisher $publisher, LoggerInterface $logger)
+    {
+        $this->publisher = $publisher;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(AMQPMessage $message)
+    {
+        try {
+            $data = unserialize($message->body);
+
+            $this->publisher->publish(
+                $data['url'],
+                $data['host'],
+                $data['uuid']
+            );
+
+            return true;
+        } catch (\Exception $e) {
+            $this->logger->error(
+                'Resource fail to be crawled',
+                [
+                    'url' => $data['url'],
+                    'host' => $data['host'],
+                ]
+            );
+
+            return false;
+        }
+    }
+}
