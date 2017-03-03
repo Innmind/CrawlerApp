03322c566925c18865dd94ff8d84fd0b16992bc1
Baptiste Langlade <baptouuuu@gmail.com>
2017-03-03 15:30:10 +0100

    add preview translator

---
+++ src/AppBundle/Translator/Property/HtmlPage/PreviewTranslator.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace AppBundle\Translator\Property\HtmlPage;
+
+use AppBundle\Translator\PropertyTranslatorInterface;
+use Innmind\Crawler\HttpResource;
+use Innmind\Rest\Client\Definition\Property;
+
+final class PreviewTranslator implements PropertyTranslatorInterface
+{
+    public function supports(HttpResource $resource, Property $property): bool
+    {
+        return $resource->attributes()->contains('preview');
+    }
+
+    public function translate(HttpResource $resource, Property $property)
+    {
+        return (string) $resource
+            ->attributes()
+            ->get('preview')
+            ->content();
+    }
+}
