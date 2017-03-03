9a8ccef14f81bc6b9b134a72e80fa750add83959
Baptiste Langlade <baptouuuu@gmail.com>
2017-03-03 16:53:06 +0100

    add rabbit mq bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -11,6 +11,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle,
             new Innmind\Rest\ClientBundle\InnmindRestClientBundle,
             new AppBundle\AppBundle(),
         ];
