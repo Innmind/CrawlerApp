ae73d3c93a584a15c9b1348f2f7d6dfc661ce90d
Baptiste Langlade <baptouuuu@gmail.com>
2017-03-19 13:55:48 +0100

    typo

--- src/AppBundle/Consumer/CrawlConsumer.php
+++ src/AppBundle/Consumer/CrawlConsumer.php
@@ -43,7 +43,7 @@ final class CrawlConsumer implements ConsumerInterface
     public function __construct(
         CrawlerInterface $crawler,
         PublisherInterface $publisher,
-        Linker $linker
+        Linker $linker,
         string $userAgent
     ) {
         $this->crawler = $crawler;
