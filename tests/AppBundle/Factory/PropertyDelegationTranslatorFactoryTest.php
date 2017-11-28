ac63271651bc401fa7b50a6e6208ee1e4dff6182
Baptiste Langlade <baptouuuu@gmail.com>
2017-11-28 19:28:38 +0100

    add missing assert

--- tests/AppBundle/Factory/PropertyDelegationTranslatorFactoryTest.php
+++ tests/AppBundle/Factory/PropertyDelegationTranslatorFactoryTest.php
@@ -8,6 +8,22 @@ use AppBundle\{
     Translator\PropertyTranslator,
     Translator\Property\DelegationTranslator
 };
+use Innmind\Crawler\{
+    HttpResource,
+    HttpResource\Attribute
+};
+use Innmind\Url\UrlInterface;
+use Innmind\Filesystem\MediaType\NullMediaType;
+use Innmind\Stream\Readable;
+use Innmind\Rest\Client\Definition\{
+    Property,
+    Type,
+    Access
+};
+use Innmind\Immutable\{
+    Map,
+    Set
+};
 use PHPUnit\Framework\TestCase;
 
 class PropertyDelegationTranslatorFactoryTest extends TestCase
@@ -15,9 +31,30 @@ class PropertyDelegationTranslatorFactoryTest extends TestCase
     public function testMake()
     {
         $translator = PropertyDelegationTranslatorFactory::make([
-            'foo' => $this->createMock(PropertyTranslator::class)
+            'foo' => $mock = $this->createMock(PropertyTranslator::class)
         ]);
+        $mock
+            ->expects($this->once())
+            ->method('supports')
+            ->willReturn(true);
 
         $this->assertInstanceOf(DelegationTranslator::class, $translator);
+        $this->assertTrue(
+            $translator->supports(
+                new HttpResource(
+                    $this->createMock(UrlInterface::class),
+                    new NullMediaType,
+                    new Map('string', Attribute::class),
+                    $this->createMock(Readable::class)
+                ),
+                new Property(
+                    'foo',
+                    $this->createMock(Type::class),
+                    new Access,
+                    new Set('string'),
+                    true
+                )
+            )
+        );
     }
 }
