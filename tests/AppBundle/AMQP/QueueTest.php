e4af9a1750d74e3de3eea9b93280896ceb55f8ba
Baptiste Langlade <baptouuuu@gmail.com>
2018-02-25 14:28:41 +0100

    add factory to build a queue declaration

---
+++ tests/AppBundle/AMQP/QueueTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\AppBundle\AMQP;
+
+use AppBundle\AMQP\Queue;
+use Innmind\AMQP\Model\Queue\Declaration;
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+    public function testNamed()
+    {
+        $queue = Queue::named('foo');
+
+        $this->assertInstanceOf(Declaration::class, $queue);
+        $this->assertSame('foo', $queue->name());
+        $this->assertTrue($queue->isDurable());
+    }
+}
